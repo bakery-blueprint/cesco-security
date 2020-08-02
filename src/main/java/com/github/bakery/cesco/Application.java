2e109b68add6114b903d5b19a63f367f476a9ac4
hotire <gngh0101@gmail.com>
2020-08-03 02:53:27 +0900

    Refactor application

--- src/main/java/com/github/bakery/cesco/Application.java
+++ src/main/java/com/github/bakery/cesco/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CescoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CescoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
