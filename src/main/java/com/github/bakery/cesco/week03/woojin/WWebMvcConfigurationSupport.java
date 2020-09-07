84a5a0c9b60dcc1aef4785fb8ae7cf3533eeda12
woojin.kang <woojin.kang@linecorp.com>
2020-09-07 21:42:58 +0900

    change taskExecutor in WebAsyncManager

---
+++ src/main/java/com/github/bakery/cesco/week03/woojin/WWebMvcConfigurationSupport.java
@@ -0,0 +1,18 @@
+package com.github.bakery.cesco.week03.woojin;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WWebMvcConfigurationSupport extends WebMvcConfigurationSupport {
+
+    protected void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.initialize();
+        configurer.setTaskExecutor(taskExecutor);
+    }
+
+}
